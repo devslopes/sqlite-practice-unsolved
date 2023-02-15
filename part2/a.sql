757ddf9151145da8877752cc0b4a51461939684a
jjhiggz <jonathan.higger@gmail.com>
2023-02-15 14:36:00 -0700

    fixed so that way it wont seg fault

--- part2/a.sql
+++ part2/a.sql
@@ -1,1 +1,4 @@
--- Fill out query below
+-- Below is a dummy query, if we leave it empty then the SQLITE package will throw a non-readable error
+-- Change the query to pass the test :)
+select *
+from dogs;
--- part2/b.sql
+++ part2/b.sql
@@ -1,1 +1,4 @@
--- Fill Out Query Below
+-- Below is a dummy query, if we leave it empty then the SQLITE package will throw a non-readable error
+-- Change the query to pass the test :)
+select *
+from dogs;
